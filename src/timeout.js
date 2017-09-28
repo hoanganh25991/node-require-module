45661543cdf6ea89ceb554bef10ec8d883dcafaf
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-28 12:14:52 +0700

    Define as function, THIS IS NOT CLASS/FUNCTIOn declare, this just excute function to create OBJ

--- src/timeout.js
+++ src/timeout.js
@@ -1,14 +1,34 @@
-let timeCount = 0;
+/**
+ * Definde as Object style
+ * With GLOBAL variable <-- not good
+ */
+// let timeCount = 0;
+//
+// const sharedTimeout = {
+//   store(time){
+//     timeCount += time
+//   },
+//
+//   log(){
+//     console.log(`Curr timeCount: ${timeCount}`)
+//   }
+// }
 
-const sharedTimeout = {
-  store(time){
-    timeCount += time
-  },
-
-  log(){
-    console.log(`Curr timeCount: ${timeCount}`)
+/**
+ * Define as function style, function create Object, which maintain SCOPE
+ */
+const _fSharedTimeout = function (){
+  let timeCount = 0;
+  return {
+    store(time){
+      timeCount += time
+    },
+    log(){
+      console.log(`Curr timeCount: ${timeCount}`)
+    }
   }
 }
+const sharedTimeout = _fSharedTimeout();
 
 var exports = module.exports = {
   sharedTimeout,
