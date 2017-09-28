b8ec6e3b1518403bf63ae0712cb31fd7da21ae21
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-28 12:17:17 +0700

    Define class as FUNCTION STYLE, ^^, function with this keyword inside @@

--- src/timeout.js
+++ src/timeout.js
@@ -17,18 +17,33 @@
 /**
  * Define as function style, function create Object, which maintain SCOPE
  */
-const _fSharedTimeout = function (){
-  let timeCount = 0;
-  return {
-    store(time){
-      timeCount += time
-    },
-    log(){
-      console.log(`Curr timeCount: ${timeCount}`)
-    }
+// const _fSharedTimeout = function (){
+//   let timeCount = 0;
+//   return {
+//     store(time){
+//       timeCount += time
+//     },
+//     log(){
+//       console.log(`Curr timeCount: ${timeCount}`)
+//     }
+//   }
+// }
+// const sharedTimeout = _fSharedTimeout();
+
+/**
+ * Deine class as FUNCTION STYLE
+ */
+const _fSharedTimeout = function(){
+  this.timeCount = 0;
+  this.store = (time) => {
+    this.timeCount += time
+  }
+  this.log = () => {
+    console.log(`Curr timeCount: ${this.timeCount}`)
   }
 }
-const sharedTimeout = _fSharedTimeout();
+
+const sharedTimeout = new _fSharedTimeout()
 
 var exports = module.exports = {
   sharedTimeout,
